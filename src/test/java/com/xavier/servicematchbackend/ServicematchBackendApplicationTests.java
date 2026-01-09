048f97b330dcce4756650e90f4cf7672aca81d97
Xavier Francisco <xavierfrancisco353@gmail.com>
2026-01-09 11:53:08 +0000

    first commit

---
+++ src/test/java/com/xavier/servicematchbackend/ServicematchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.servicematchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicematchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
